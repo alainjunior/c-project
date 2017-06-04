887ee632f75ea6716a5b8a1ea48547dc61d7fbab
betanets <can3456@mail.ru>
2017-06-04 11:26:57 +0700

    Исправлен шаблон страницы отзывов

--- web/otziv.php
+++ web/otziv.php
@@ -1,3 +1,7 @@
+<?php
+	include 'base.php';
+?>
+
 <html>
 <head>
 <title>ООО «ПРОКС»</title>
@@ -19,78 +23,44 @@
 </head>
 
 <body>
-	<div class="head">
-		<div class="header">
-			<a href="index.html"><img src="images/logo.jpg" width="210" height="120" alt="I"></a>
-			<div class="me">
-				<p2>ПРОКС</p2><p3> / БАРНАУЛ<p3>
-				<div class="kons">
-				<td><p style="text-align: center"><a href="konsult.html"><button class="konsult">Онлайн консультация</button></a></p></td>
+
+	<div class="col-sm-offset-0 col-sm-12 col-md-offset-0 col-md-12 col-lg-offset-1 col-lg-10" style="margin-top: 50px">
+		<div class="col-sm-6 col-md-6 col-lg-8">
+			<div class="panel panel-default">
+				<div class="panel-body text-center">
+				<!-- возможно, каждый из блоков отзыва стоит формировать в отдельном php -->
+				<!-- нужны особые стили -->
+					<h2>Отзывы клиентов</h2>
+					<hr size="0.01" width="100%">
+					<b>ФИО клиента<br>
+					ООО "1"<br>
+					30.06.2016</b><br><br>
+					<p4>Хочу выразить благодарность нашему партнеру "1С:Сопровождение. ПРОКС" текст отзыва </p4>
+					<br><br>
+					<p4> еще текст</p4>
+					<hr size="0.01" width="100%">
+					<br>
+					<b>ФИО клиента<br>
+					ООО "2"<br>
+					08.04.2016</b><br><br>
+					<p4>текст отзыва</p4>
+					<hr size="0.01" width="100%">
+					<br>
+					<b>ФИО клиента<br>
+					ООО "2"<br>
+					15.01.2016</b><br><br>
+					<p4>текст отзыва</p4>  	
 				</div>
-				<br>
-				<hr size="0.2" width="740">
-				<ul id="menu">
-				<li><a href="compani.html">О нас</a></li>
-				<li>
-					<a href="*">Наши услуги</a>
-						<ul>
-							<li><a href="program_prod.html">Программные продукты</a></li>
-							<li><a href="servis.html">Сервисы</a></li>
-							<li><a href="its.html">Сопровождение</a></li>
-							
-						</ul>
-				</li>
-				<li><a href="otziv.html">Отзывы</a></li>
-				<li><a href="contacts.html">Контакты</a></li>
-				<li><a href="faq.html">Частые вопросы</a></li>
-				</ul>
-				
-			</div>
-		</div>
-			<img src="images/gran.png" width="100%" height="20" alt="I"></img>
-	
-	<div class="cals">
-		<div class="otz">
-			<h1>Отзывы клиентов</h1>
-			<hr size="0.01" width="100%">
-			
-			<b>ФИО клиента<br>
-			ООО "1"<br>
-			30.06.2016</b><br><br>
-			<p4>Хочу выразить благодарность нашему партнеру "1С:Сопровождение. ПРОКС" текст отзыва </p4>
-			<br><br>
-			<p4> еще текст</p4>
-			<hr size="0.01" width="100%">
-			<br>
-			<b>ФИО клиента<br>
-			ООО "2"<br>
-			08.04.2016</b><br><br>
-			<p4>текст отзыва</p4>
-			<hr size="0.01" width="100%">
-			<br>
-			<b>ФИО клиента<br>
-			ООО "2"<br>
-			15.01.2016</b><br><br>
-			<p4>текст отзыва</p4>  			
-		</div>
-		<div class="pb">
-			<div class="osn">
-				<figure class="sign">
-				<figcaption><h3>Контакты</h3></figcaption>
-				<hr size="0.01" width="100%">
-				0656043, г. Барнаул,<br>
-				ул. Пролетарская, 90<br>
-				+7 (3852) 229-050
-				<br><br>
-				Как нас найти:<br><br>
-				<img src="images/karta.png" width="280" height="240" alt="I">
-				</figure>
 			</div>
 		</div>
 		
+		<?php
+			include 'contacts-small-panel.php';
 		
+		?>
 	</div>
-	
+
+	<!--
 	<div class="footer">
 		<div class="foot">
 			© ООО «ПРОКС»<br>
@@ -99,7 +69,7 @@
 			+7 (3852) 229-444. 
 		</div>
 	</div>
-
+-->
 
 </body>
 </html>
