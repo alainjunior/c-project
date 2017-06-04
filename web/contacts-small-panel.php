1936c3bc91bbfcbc871c198852ec2ccf09b38a69
betanets <can3456@mail.ru>
2017-06-04 11:21:48 +0700

    Создан шаблон страницы "О компании" и маленький виджет контактов

---
+++ web/contacts-small-panel.php
@@ -0,0 +1,17 @@
+﻿<div class="col-sm-6 col-md-6 col-lg-4">
+	<div class="panel panel-default" style="max-height: 640px; min-height: 640px;">
+		<div class="panel-body text-center">
+			<h2>Контакты</h2>
+			<hr size="0.01" width="100%">
+			<h4>Наш адрес</h4>
+			<div>
+			656043, г. Барнаул, ул. Пролетарская, д.90<br />
+			+7 (3852) 229-444<br />
+			</div>
+			<hr size="0.01" width="100%">
+			<?php
+				include '2gis.php';
+			?>
+		</div>
+	</div>
+</div>
