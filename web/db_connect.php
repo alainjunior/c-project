8121e6c35c1416ddd6e8bd9a75cfb6f0390a154b
betanets <can3456@mail.ru>
2017-06-04 14:45:15 +0700

    Подключение к БД переведено на mysqli, изменен вид главной страницы, добавлена карусель на главную страницу, добавлены стандартные глифы bootstrap

--- web/db_connect.php
+++ web/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 		// 	Соединяемся, выбираем базу данных (хост, логин, пароль)
-		$link = mysql_pconnect('localhost', 'root', '') or die('Не удалось соединиться: ' . mysql_error());
+		$link = mysqli_connect('localhost', 'root', '') or die('Не удалось соединиться: ' . mysql_error());
 		//	echo 'Соединение успешно установлено';
-		mysql_select_db('prox') or die('Не удалось выбрать базу данных');
+		mysqli_select_db($link, 'prox') or die('Не удалось выбрать базу данных');
 		
 	?>
---
+++ web/news-small-panel.php
@@ -0,0 +1,16 @@
+﻿<div class="col-sm-6 col-md-6 col-lg-4">
+	<div class="panel panel-default">
+		<div class="panel-body text-center">
+			<h2>Новости</h2>
+			<hr size="0.01" width="100%">
+			01.01.2017<br>
+			<a href="*">Онлайн-кассы. Подключение к ОФД. 54-ФЗ</a>
+			<hr size="0.01" width="100%">
+			01.01.2017<br>
+			<a href="*">День 1С: Карьеры</a>
+			<hr size="0.01" width="100%">
+			01.01.2016<br>
+			<a href="*">3-НДФЛ за 2016 год! Начинаем декларационную компанию.</a>
+		</div>
+	</div>
+</div>
