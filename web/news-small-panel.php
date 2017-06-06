a515a5619544d49857188e4e170cfae2aff108f4
kamu22rus <romchik_sk@mail.ru>
2017-06-06 23:43:27 +0700

    news

--- web/news-small-panel.php
+++ web/news-small-panel.php
@@ -2,15 +2,18 @@
 	<div class="panel panel-default">
 		<div class="panel-body text-center">
 			<h2>Новости</h2>
-			<hr size="0.01" width="100%">
-			01.01.2017<br>
-			<a href="*">Онлайн-кассы. Подключение к ОФД. 54-ФЗ</a>
-			<hr size="0.01" width="100%">
-			01.01.2017<br>
-			<a href="*">День 1С: Карьеры</a>
-			<hr size="0.01" width="100%">
-			01.01.2016<br>
-			<a href="*">3-НДФЛ за 2016 год! Начинаем декларационную компанию.</a>
+			<?php
+				$sql = "SELECT id, title, date FROM news where deleted=\"0\" and published=\"1\" ORDER BY date DESC LIMIT 3";
+				$result = $link->query($sql);
+
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+						echo "<hr size=\"0.01\" width=\"100%\">".date_create_from_format('Y-m-d H:i:s', $row["date"])->format('Y-m-d')."<br><a href=\"#\">".$row["title"]."</a>";
+					}
+				} else {
+					echo "Нет новостей";
+				}
+			?>
 		</div>
 	</div>
 </div>
